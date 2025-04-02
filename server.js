f4218b96a7783193da4d65cf9e19d69a7c2e9d32
Sembatya2020 <ivansmbt@gmail.com>
2025-04-02 10:52:53 +0300

    week04

--- server.js
+++ server.js
@@ -14,6 +14,34 @@ const baseController = require('./controllers/baseController');
 const inventoryRoute = require('./routes/inventoryRoute');
 const utilities = require('./utilities/');
 const errorRoute = require("./routes/errorRoute");
+const session = require("express-session")
+const pool = require('./database/')
+const accountRoute = require("./routes/accountRoute")
+const bodyParser = require("body-parser");
+
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
  
 /* ***********************
  * Routes
@@ -23,6 +51,11 @@ app.use(expressLayouts);
 app.set('layout', './layouts/layout'); // n
 app.use(static);
 app.use("/error", errorRoute);
+app.use("/account", accountRoute)
+app.use("/registers", accountRoute)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
  
 /* ***********************
  * Homepage Route
